dccf11d5d9861d9b129cdeb7b34d5b83402a061d
Jeremy Shaw <jeremy@n-heptane.com>
2011-02-21 15:41:22 -0600

    Added bigtable/happstack-hsp.hs

---
+++ bigtable/happstack-hsp.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -F -pgmFtrhsx #-}
+module Main where
+
+import Happstack.Server
+import Happstack.Server.HSP.HTML
+
+bigTable :: ServerPart XML
+bigTable =
+    defaultTemplate "BigTable" ()
+     <table>
+      <% [ <tr>
+             <th><% row %></th>
+             <% [<td><% col %></td> | col <- cols ] %>
+           </tr> 
+           | row <- rows ] 
+       %>
+     </table>
+    where
+      rows, cols :: [Int]
+      rows = [1..10]
+      cols = [1..10]
+
+main = simpleHTTP nullConf bigTable
